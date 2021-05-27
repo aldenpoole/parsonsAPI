593a0427c6b11ead5e8d595c53ed9f061eaf83a3
aldenpoole <62851994+aldenpoole@users.noreply.github.com>
2021-05-27 09:26:37 -0500

    Update trajectories.js

--- routes/trajectories.js
+++ routes/trajectories.js
@@ -20,12 +20,25 @@ router.delete('/', (req, res) => {
 router.get('/', (req, res) => {
     res.send(trajectories);
 });
-
+//get trajectories based on id number... working
 router.get('/:id', (req, res) => {
     const { id } = req.params;
 
     const foundTrajectory = trajectories.find((trajectory)=> trajectory.id == id);
     res.send(foundTrajectory);
+});
+//get trajectories based on launch phase... not working
+router.get('/:type', (req, res) => {
+    var type = req.params;
+
+    var foundTrajectories = [] 
+    var i =0;
+    for(i = 0; i < trajectories.length;i++){
+        if(toString(trajectories.type[i]) == toString(type)){
+        foundTrajectories.push(trajectories[i]);
+        }
+    }
+    res.send(foundTrajectories);
 });
 
 export default router;
