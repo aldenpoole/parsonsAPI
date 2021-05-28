473fc702f891dff279e2d57403d59b89e04b78c0
aldenpoole <62851994+aldenpoole@users.noreply.github.com>
2021-05-28 09:58:11 -0500

    Update deleteData.js

--- deleteData.js
+++ deleteData.js
@@ -1,6 +1,7 @@
 //delData.js deletes all objects stored in API
 
 import fetch from 'node-fetch'
+const fetcher = 'http://localhost:5000/trajectories/'
 
 //GET method
 const getMethod = {
@@ -20,13 +21,13 @@ const deleteMethod = {
   
    //use GET method to loop through trajectory list and send
    //a DELETE method for each entry.
-   fetch('http://localhost:5000/trajectories/', getMethod) 
+   fetch(fetcher, getMethod) 
    .then(response => response.json())
    .then((responseJson) => {
     var i;
     for(i =0; i< (responseJson).length; i++){
         //fetch statement not working??
-        fetch('http://localhost:5000/trajectories/', deleteMethod)
+        fetch(fetcher, deleteMethod)
         console.log((i+1) + " deleted.")
     }
     console.log("All entries deleted.")
