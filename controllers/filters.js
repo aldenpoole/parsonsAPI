b1bfe14516c940de89beb3594237de1c00a33fab
aldenpoole <62851994+aldenpoole@users.noreply.github.com>
2021-05-28 09:53:17 -0500

    Create filters.js

---
+++ controllers/filters.js
@@ -0,0 +1,22 @@
+let filters = []
+
+export const createData = (req, res) => {
+    /*const trajectory = req.body;
+    trajectories.push(trajectory);
+    res.send(`Trajectory with ID ${trajectory.id} added.`);*/
+}
+
+export const deleteData = (req, res) => {
+    //trajectories =[];
+}
+
+export const getData = (req, res) => {
+    //res.send(trajectories);
+}
+
+export const getDataByID = (req, res) => {
+   /* const { id } = req.params;
+
+    const foundTrajectory = trajectories.find((trajectory)=> trajectory.id == id);
+    res.send(foundTrajectory);*/
+}
