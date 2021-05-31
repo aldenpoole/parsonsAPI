ff6b5ad52e2f4ff576eabd1ae6a9d06558cacad6
aldenpoole <62851994+aldenpoole@users.noreply.github.com>
2021-05-31 10:50:08 -0500

    Update trajectories.js

--- routes/trajectories/trajectories.js
+++ routes/trajectories/trajectories.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createData, deleteData, getData, getDataByID } from '../../controllers/trajectories.js';
+import { createData, deleteData, getData, getDataByID, getDataByStage } from '../../controllers/trajectories.js';
 
 const router = express.Router();
 
@@ -12,5 +12,7 @@ router
     
 router
     .get('/:id', getDataByID);
+//??
+router.get('/?type=final', getDataByStage);
 
 export default router;
