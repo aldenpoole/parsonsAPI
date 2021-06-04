347a97e27dcdb4740e077d6e8ea048f766305ba3
aldenpoole <62851994+aldenpoole@users.noreply.github.com>
2021-06-04 12:29:59 -0500

    Update trajectories.js

--- controllers/trajectories.js
+++ controllers/trajectories.js
@@ -33,15 +33,3 @@ export const getDataByID = (req, res) => {
     res.send(foundTrajectory);
 }
 
-export const getDataByStage = (req, res) => {
-    const { stage } = req.params;
-    var i = 0;
-    const foundTrajectories = [];
-    for(i =0; i < trajectories.length; i++){
-        if(trajectories[i].type == stage){
-            foundTrajectories =+ trajectories[i];
-        }
-    }
-    //const foundTrajectory = trajectories.find((trajectory)=> trajectory.type == stage);
-    res.send(foundTrajectories);
-}
